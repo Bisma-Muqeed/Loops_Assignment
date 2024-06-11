6b47131a5a98efcd8e718b54a717ebe65ba78587
Bisma Muqeed <bismamuqeed@gmail.com>
2024-06-11 05:27:50 -0700

    loop_assignment 3  loop_assignment 3

---
+++ loop_assignment no 3/loop3.js
@@ -0,0 +1,10 @@
+// Counter Incrementer (Using do while loop ) loop assignment 3
+// Set the starting counter to 0:
+var counter = 0;
+// Create a variable, step, to increase your counter by:
+var step = 4;
+// Add a do...while loop:
+do {
+    console.log("Counter:", counter);
+    counter += step;
+} while (counter <= 10); // Continue to loop until the counter is less than or equal to 10
